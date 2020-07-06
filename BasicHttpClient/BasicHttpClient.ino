79cf9cb7d80c2b064131f6f224207c8e8100750c
Amaury Semery <amaury.semery@laposte.net>
2020-07-06 14:15:22 +0200

    Update BasicHttpClient.ino

--- BasicHttpClient/BasicHttpClient.ino
+++ BasicHttpClient/BasicHttpClient.ino
@@ -1,9 +1,9 @@
 /**
- * BasicHTTPClient.ino
- *
- *  Created on: 24.05.2015
- *
- */
+   BasicHTTPClient.ino
+
+    Created on: 24.05.2015
+
+*/
 
 #include <Arduino.h>
 
@@ -17,63 +17,63 @@
 WiFiMulti wifiMulti;
 
 void setup() {
-  
 
-    pinMode(4,OUTPUT);
-    digitalWrite(4,LOW);
-    USE_SERIAL.begin(115200);
 
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-    USE_SERIAL.println();
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);
+  USE_SERIAL.begin(115200);
 
-    for(uint8_t t = 4; t > 0; t--) {
-        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
-        USE_SERIAL.flush();
-        delay(1000);
-    }
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+  USE_SERIAL.println();
 
-    wifiMulti.addAP("POP_SENSORS", "P0PS3NS0RS!");
+  for (uint8_t t = 4; t > 0; t--) {
+    USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
+    USE_SERIAL.flush();
+    delay(1000);
+  }
+
+  wifiMulti.addAP("POP_SENSORS", "P0PS3NS0RS!");
 
 }
 
 void loop() {
-    // wait for WiFi connection
-    if((wifiMulti.run() == WL_CONNECTED)) { // Si c'est connecté, ça fait ce qu'il y à en dessous
+  // wait for WiFi connection
+  if ((wifiMulti.run() == WL_CONNECTED)) { // Si c'est connecté, ça fait ce qu'il y à en dessous
 
-        HTTPClient http; // va créer un objet qui s'appelle HTTPClient qui va permettre de lancer des requêtes en HTTP
+    HTTPClient http; // va créer un objet qui s'appelle HTTPClient qui va permettre de lancer des requêtes en HTTP
 
-        USE_SERIAL.print("[HTTP] begin...\n");
-        // configure traged server and url
-        //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
-        http.begin("http://192.168.1.200:1883/T/23"); //HTTP => démarre connexion vers le serveur mentionné
+    USE_SERIAL.print("[HTTP] begin...\n");
+    // configure traged server and url
+    //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
+    http.begin("http://192.168.1.200:1883/T/23"); //HTTP => démarre connexion vers le serveur mentionné
 
-        USE_SERIAL.print("[HTTP] GET...\n");
-        // start connection and send HTTP header
-        int httpCode = http.GET(); // soumet une requête de type "get", puis récupère résultat qui sera collée dans HTTP
+    USE_SERIAL.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET(); // soumet une requête de type "get", puis récupère résultat qui sera collée dans HTTP
 
-        // httpCode will be negative on error
-        if(httpCode > 0) { // si c'est supérieur à 0, il refait un test)
-            // HTTP header has been send and Server response header has been handled
-            USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+    // httpCode will be negative on error
+    if (httpCode > 0) { // si c'est supérieur à 0, il refait un test)
+      // HTTP header has been send and Server response header has been handled
+      USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
 
-            // file found at server
-            if(httpCode == HTTP_CODE_OK) { // Si ça s'est bien passé, il refait une variable où il met le getString puis affiche à l'écran => on a reçu un code 200
-                String payload = http.getString();
-                USE_SERIAL.println(payload);
-                if (payload=="ON"){
-                  digitalWrite(4,HIGH);
-                  }
-                if (payload=="OFF"){
-                  digitalWrite(4,LOW);
-                  }
-            }
-        } else { // sinon, il dit que ça ne fonctionne pas
-            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) { // Si ça s'est bien passé, il refait une variable où il met le getString puis affiche à l'écran => on a reçu un code 200
+        String payload = http.getString();
+        USE_SERIAL.println(payload);
+        if (payload == "ON") {
+          digitalWrite(4, HIGH);
         }
-
-        http.end();
+        if (payload == "OFF") {
+          digitalWrite(4, LOW);
+        }
+      }
+    } else { // sinon, il dit que ça ne fonctionne pas
+      USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
 
-    delay(5000); // le tout est fait toutes les 5 secondes.
+    http.end();
+  }
+
+  delay(5000); // le tout est fait toutes les 5 secondes.
 }
